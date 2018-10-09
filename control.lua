425bb2c384f7bf312649af4568ceadbf5f86843e
Daniel Grace <thisgenericname@gmail.com>
2018-10-08 17:18:23 -0700

    Recover partially-used battery packs when mining vehicles.

--- control.lua
+++ control.lua
@@ -1,5 +1,3 @@
-do return end
-
 config = require "config"
 
 local function qis_register_events()
@@ -33,16 +31,47 @@ local function qis_apply_patch()
 end
 
 local function on_init()
-    qis_register_events()
-    qis_apply_patch()
+--    qis_register_events()
+--    qis_apply_patch()
 end
 
 local function on_load()
-    qis_register_events()
+--    qis_register_events()
 end
-
-
 
 script.on_init(on_init)
 script.on_configuration_changed(on_init)
 script.on_load(on_load)
+
+-- Register events.
+local function has_recoverable_battery(entity)
+    local burner  = entity.burner
+    return (
+        burner and -- Has a burner
+        burner.currently_burning and -- Is currently burning fuel
+        burner.currently_burning.fuel_category == 'stored-electricity' and -- Is our fuel type
+        burner.currently_burning.burnt_result  -- Has a burnt result?
+    )
+end
+
+local function recover_battery(event)
+    if has_recoverable_battery(event.entity) then
+        event.buffer.insert({name=event.entity.burner.currently_burning.burnt_result.name, count=1})
+    end
+end
+
+local function pre_recover_battery(event)
+    if has_recoverable_battery(event.entity) then
+        event.entity.burner.burnt_result_inventory.insert({name=event.entity.burner.currently_burning.burnt_result.name, count=1})
+        event.entity.burner.currently_burning = nil
+    end
+end
+
+
+
+
+script.on_event(defines.events.on_player_mined_entity, recover_battery)
+script.on_event(defines.events.on_robot_mined_entity, recover_battery)
+--script.on_event(defines.events.on_pre_player_mined_item, pre_recover_battery)
+--script.on_event(defines.events.on_robot_pre_mined, pre_recover_battery)
+-- game.print("Done with control.lua")
